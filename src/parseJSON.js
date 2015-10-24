db68d3acc08620af233b28649e20b71b400463de
Erik Brown <erikdbrown@gmail.com>
2015-10-24 16:41:12 -0700

    parseJSON is working in isolation, but is forcing SpecRunner into an infinite loop. I suspect it's because it's not yet handling numbers an boolean

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,84 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
+
+  var position = 0;
+  // function to look at the next value
+  var next = function(charPosition) {
+    var start = json.charAt(charPosition);
+    return json.charAt(charPosition + 1);
+  }
+
+  // funciton to traverse the string until it finds specific markers
+  // { start of an object
+  // [ start of an object
+  // ' start of a string
+  // : end of a key
+  // , end of a value
+
+  var whatIsIt = function(pos) {
+    position = pos;
+    var startValue = json.charAt(position);
+    position++;
+    if (startValue === '{') {
+      return objectParser(position);
+    } else if (startValue === "'" || startValue === '"') {
+      return stringParser(position);
+    } else if (startValue === '[') {
+      return arrayParser(position);
+    } else if (startValue === '{') {
+      return objectParser(position);
+    }
+  }
+
+  var stringParser = function() {
+    var string = json.charAt(position);
+
+    while (next(position) !== '"' && next(position) !== "'") {
+      string += next(position);
+      position++;
+    }
+    return string;
+  }
+
+
+// {"menu":{"id":"file","value":"File","popup":{"menuitem":[{"value":"New","onclick":"CreateNewDoc()"},{"value":"Open","onclick":"OpenDoc()"},{"value":"Close","onclick":"CloseDoc()"}]}}}"
+  // parses through an string and returns the array
+  var arrayParser = function() {
+    var returnArray = [];
+    var value = '';
+
+    while (next(position) !== ']') {
+        value = whatIsIt(position);
+        position++;
+        if (next(position) === ',') {
+          returnArray.push(value);
+          position += 2;
+        }
+      }
+    return returnArray;
+  }
+
+
+  var objectParser = function() {
+    var returnObject = {};
+    var key = '';
+    var value;
+
+    while (next(position) !== '}') {
+      key = whatIsIt(position); // position is the last value before quotation
+      position += 3; // moves it to the value after ':'
+      returnObject[key] = whatIsIt(position); // position is the last value of the item
+      position++; // moves position to the close puncutation
+      if (next(position) === ',') {
+        position += 2;
+      }
+    }
+    return returnObject;
+  };
+
+  return whatIsIt(0);
+
+
   // your code goes here
 };
