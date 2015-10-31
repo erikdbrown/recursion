0239c6da87455a432416989ff6cdd90fbd68b40e
Erik Brown <erikdbrown@gmail.com>
2015-10-31 16:35:43 -0700

    added comments. recurses through the entire DOM rather than starting with $('body')

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,15 +6,35 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className) {
 
+	// get ElementByClassName returns an array of elements on a DOM that
+	// have the className entered as a parameter. This re-write uses
+	// recusion and jquery to traverse a DOM and push any element
+	// containg the className parameter into an returnArray.
+	// ================================================================
+
+	// sets a return array.
 	var returnArray =[];
+
+	// recursive function findChildren take in an array of element on a
+	// DOM
+	// ===============================================================
+	// Things to note: 
+	// 1. element.children() will pull all the child elements into an array. 
+	// 2. If element.children().length === 0, there are no children. 
+	// 3. element.hasClass('className') returns a boolean
+	// ===============================================================
+	// The function loops through an array of elements to indentify if
+	// that level of elements contains an element with the className
+	// parameter.
+	// If an element.hasClass(className) === true, it pushes that element into the returnArray.
 
 	var findChildren = function(element) {
 
-		// termination
+		// termination statement when there are no elements at that level or organization.
 		if (element.length === 0) {
 			return
 
-		// checks through each element to check class
+		// This for loop checks through each element to check class
 		// if element has a class, push the class to returnArray
 		} else {
 			for (var i = 0; i < element.length; i++) {
@@ -25,12 +45,10 @@ var getElementsByClassName = function(className) {
 		}
 
 		// recursion: run through findChildren on element.children()
-		if (element.children().length > 0) {
-			findChildren(element.children());
-		}
+		findChildren(element.children());
 	}
 
-	// start recursion by setting element as $('body')
-	findChildren($('body'));
+	// start recursion by setting element as $(document)
+	findChildren($(document));
 	return returnArray;
 };
